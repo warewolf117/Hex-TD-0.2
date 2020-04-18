5dc87fef025912ed5b4122a484143e4fde2b8f98
Erzihark <63552221+Erzihark@users.noreply.github.com>
2020-04-18 18:13:10 -0500

    Added pause menu

---
+++ Hex TD 0.2/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;//for loading and reloading scenes
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject ui;
+    public GameObject pauseButton;
+
+    public void TogglePause()
+    {
+        ui.SetActive(true);
+        pauseButton.SetActive(false);
+        if (ui.activeSelf)
+        {
+            Time.timeScale = 0f;
+        }
+    }
+    public void ToggleResume()
+    {
+        ui.SetActive(false);
+        pauseButton.SetActive(true);
+        if (!ui.activeSelf)
+        {
+            Time.timeScale = 1f;
+        }
+    }
+
+    public void Retry()
+    {
+        Time.timeScale = 1f;
+        //scenemanager.loadscene() selects the scene to load, in this we load the current scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Menu ()
+    {
+
+    }
+
+    void Update ()
+    {
+
+    }
+}
