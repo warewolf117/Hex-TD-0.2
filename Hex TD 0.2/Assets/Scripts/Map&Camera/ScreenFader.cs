7f7724de8e3a4863cdbd0062e4c98524b09897c5
Erzihark <63552221+Erzihark@users.noreply.github.com>
2020-04-22 13:31:52 -0500

    ui tweaks

---
+++ Hex TD 0.2/Assets/Scripts/Map&Camera/ScreenFader.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScreenFader : MonoBehaviour
+{
+    public Image Image;
+    public AnimationCurve curve;
+
+    private void Start()
+    {
+       StartCoroutine(FadeIn());
+    }
+
+    public void FadeTo(string scene)
+    {
+        StartCoroutine(FadeOut(scene));
+    }
+
+    IEnumerator FadeIn()
+    {
+        float t = 1f;
+
+        while (t > 0)
+        {
+            t -= Time.deltaTime * 0.5f;
+            float a = curve.Evaluate(t); //controls the curve
+            Image.color = new Color(0f, 0f, 0f, a); // this modifies the alpha value
+            yield return 0; //skips to the next frame
+        }
+        //load a scene
+    }
+
+    IEnumerator FadeOut(string scene)
+    {
+        float t = 0f;
+
+        while (t < 1f)
+        {
+            t += Time.deltaTime * 0.5f;
+            float a = curve.Evaluate(t); //controls the curve
+            Image.color = new Color(0f, 0f, 0f, a); // this modifies the alpha value
+            yield return 0; //skips to the next frame
+        }
+        //load a scene
+
+        SceneManager.LoadScene(scene);
+    }
+
+}
