3f0f8e1aceb8a5352765bf679a1c2149bb8b9fb0
warewolf117 <63457627+warewolf117@users.noreply.github.com>
2020-04-22 14:05:39 -0400

    Camera settings

--- Hex TD 0.2/Assets/Scripts/TargetFrameRate.cs
+++ Hex TD 0.2/Assets/Scripts/TargetFrameRate.cs
@@ -8,7 +8,7 @@ public class TargetFrameRate : MonoBehaviour
     void Start()
     {
         QualitySettings.vSyncCount = 0;
-        Application.targetFrameRate = 60;
+        Application.targetFrameRate = 30;
     }
 
     // Update is called once per frame
