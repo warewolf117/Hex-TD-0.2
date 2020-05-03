689361d8d81895e4f8d6338dc3383d790da4a7f4
Erzihark <63552221+Erzihark@users.noreply.github.com>
2020-05-03 11:23:52 -0500

    Fixed Pooler

---
+++ Hex TD 0.2/Assets/Scripts/BulletPooler.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class BulletPooler : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+
+    private Queue<GameObject> availabelObjects = new Queue<GameObject>();
+
+    public static BulletPooler Instance
+    {
+        get;
+        private set;
+    }
+
+    private void Awake()
+    {
+        Instance = this;
+        GrowPool();
+    }
+
+    public GameObject GetFromPool()
+    {
+        if (availabelObjects.Count == 0)
+            GrowPool();
+
+            var instance = availabelObjects.Dequeue();
+            instance.SetActive(true);
+            return instance;
+        
+    }
+
+    private void GrowPool()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            var instanceToAdd = Instantiate(bulletPrefab);
+            instanceToAdd.transform.SetParent(transform);
+            AddToPool(instanceToAdd);
+        }
+    }
+
+    public void AddToPool(GameObject instance)
+    {
+        instance.SetActive(false);
+        availabelObjects.Enqueue(instance);
+    }
+
+
+
+
+}
