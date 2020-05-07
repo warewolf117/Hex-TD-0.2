b30ae3e552db99c63fac1d0d52ac520e690a2bf1
warewolf117 <63457627+warewolf117@users.noreply.github.com>
2020-05-07 16:47:40 -0400

    Camera updated, some materials moved, added the neon mountains

--- Hex TD 0.2/Assets/Scripts/Map&Camera/MobileCameraControlBackup.cs
+++ Hex TD 0.2/Assets/Scripts/Map&Camera/MobileCameraControlBackup.cs
@@ -9,41 +9,29 @@ public class MobileCameraControlBackup : MonoBehaviour
 {
 
     private static readonly float PanSpeed = 1f;
-    private static readonly float ZoomSpeedTouch = 5f;
-    private static readonly float ZoomSpeedMouse = 5f;
 
     private static readonly float[] BoundsX = new float[] { -3f, 3f };
     private static readonly float[] BoundsZ = new float[] { -12f, -6f };
-    private static readonly float[] ZoomBounds = new float[] { 20f, 80f };
 
     private Camera cam;
     public GameObject target;
-    public float CurrentFOV;
     public int CurrentPosition; // 0 = far out, 1=zoomed in hexes. 2=zoomed in lane
-    private bool ZoomingIn;
-    private bool ZoomingOut;
-    private float OldFOV;
-    private bool zoomtrue;
-
-
+    private float CurrentFOV;
+    private bool zoomingin;
+    private bool zoomingout;
     private Vector3 lastPanPosition;
     private int panFingerId; // Touch mode only
 
-    private bool wasZoomingLastFrame; // Touch mode only
-    private Vector2[] lastZoomPositions; // Touch mode only
 
 
-    
+
     void Start()
     {
-        CurrentFOV = cam.fieldOfView;
         CurrentPosition = 0;
-        ZoomingOut = false;
-        ZoomingIn = false;
         Screen.SetResolution(1280, 720, true);
         // cam.aspect = 16 / 9;
     }
-        void Awake()
+    void Awake()
     {
         cam = GetComponent<Camera>();
     }
@@ -51,8 +39,6 @@ public class MobileCameraControlBackup : MonoBehaviour
     void Update()
     {
         CurrentFOV = cam.fieldOfView;
-        
-
         if (Input.touchSupported && Application.platform != RuntimePlatform.WebGLPlayer)
         {
             HandleTouch();
@@ -62,51 +48,38 @@ public class MobileCameraControlBackup : MonoBehaviour
             HandleMouse();
         }
 
-
-        if ( CurrentFOV >= 80f && CurrentPosition == 1 && ZoomingIn == true && CurrentPosition != 2)
+        if (CurrentPosition == 0)
         {
-            CurrentPosition = 2;
-            ZoomingIn = false;
-            ZoomingOut = false;
-
+            cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, 80.5f, 0.2f);
         }
 
-        if (CurrentFOV <= 37f && CurrentFOV >= 36f && CurrentPosition != 1)
-        {
-            
-            CurrentPosition = 1;
-            ZoomingIn = false;
-            ZoomingOut = false;
 
+
+        if (CurrentPosition == 1 && zoomingout == true)
+        {
+             Vector3 StartPosition2 = cam.transform.position;
+
+             Vector3 move2 = new Vector3(0f, 18.5f, -7f);
+
+             cam.transform.position = Vector3.Lerp(StartPosition2, move2, 0.2f);
+
+             Quaternion StartRotation2 = cam.transform.rotation;
+
+             Quaternion rotate2 = Quaternion.Euler(70f, 0f, 0f);
+
+             cam.transform.rotation = Quaternion.Lerp(StartRotation2, rotate2, 0.2f);
+
+             cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, 36.5f, 0.2f);
         }
 
-        if (CurrentFOV >= 80f && CurrentPosition != 0 && ZoomingOut == true && CurrentPosition != 2 )
-        {
-            CurrentPosition = 0;
-            ZoomingIn = false;
-            ZoomingOut = false;
-
-        }
-
-        if (CurrentPosition == 0 && ZoomingOut == false && ZoomingIn == true && zoomtrue == true)
+        if (CurrentPosition == 1)
         {
             cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, 36.5f, 0.2f);
-            zoomtrue = false;
-
             return;
         }
 
-        if (CurrentPosition == 1 && ZoomingIn == false && ZoomingOut == true && zoomtrue == true)
+        if (CurrentPosition == 2 && zoomingin == true)
         {
-            cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, 80.5f, 0.2f);
-            zoomtrue = false;
-            return;
-        }
-
-        if (CurrentPosition == 1 && ZoomingIn == true && ZoomingOut == false && zoomtrue == true)
-        {
-            cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, 80.5f, 0.2f);
-
             Vector3 StartPosition = cam.transform.position;
 
             Vector3 move1 = new Vector3(0f, 3f, 0f);
@@ -118,43 +91,69 @@ public class MobileCameraControlBackup : MonoBehaviour
             Quaternion rotate1 = Quaternion.Euler(15f, 30f, 0f);
 
             cam.transform.rotation = Quaternion.Lerp(StartRotation, rotate1, 0.2f);
-            zoomtrue = false;
-            return;
-        }
 
-        if (CurrentPosition == 2 && ZoomingIn == false && ZoomingOut == true && zoomtrue == true)
+            cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, 80.5f, 0.2f);
+        }
+        if (CurrentPosition == 0 && CurrentFOV >= 80f)
         {
-            cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, 36.5f, 0.2f);
+            zoomingout = false;
+        }
 
-            Vector3 StartPosition2 = cam.transform.position;
+        if (CurrentPosition == 1 && CurrentFOV >= 36f)
+        {
+            zoomingout = false;
+        }
+        if (CurrentPosition == 2 && CurrentFOV >= 80f)
+        {
+            zoomingin = false;
+        }
 
-            Vector3 move2 = new Vector3(0f, 18.5f, -7f);
 
-            cam.transform.position = Vector3.Lerp(StartPosition2, move2, 0.2f);
 
-            Quaternion StartRotation2 = cam.transform.rotation;
 
-            Quaternion rotate2 = Quaternion.Euler(70f, 0f, 0f);
+    }
 
-            cam.transform.rotation = Quaternion.Lerp(StartRotation2, rotate2, 0.2f);
-            zoomtrue = false;
+
+    public void ZoomIn()
+    {    
+
+        if (CurrentPosition == 0)
+        {
+            CurrentPosition = 1;
+            zoomingin = true;
             return;
         }
 
-      
-        
+        if (CurrentPosition == 1)
+        {
+            CurrentPosition = 2;
+            zoomingin = true;
+        }
+    }
 
+    public void ZoomOut()
+    {
 
+        if (CurrentPosition == 1)
+        {
+            CurrentPosition = 0;
+            zoomingout = true;
+            return;
+        }
+
+        if (CurrentPosition == 2)
+        {
+            CurrentPosition = 1;
+            zoomingout = true;
+
+        }
     }
 
     void HandleTouch()
     {
         switch (Input.touchCount)
         {
-
             case 1: // Panning
-                wasZoomingLastFrame = false;
-
                 // If the touch began, capture its position and its finger ID.
                 // Otherwise, if the finger ID of the touch doesn't match, skip it.
                 Touch touch = Input.GetTouch(0);
@@ -167,31 +166,6 @@ public class MobileCameraControlBackup : MonoBehaviour
                 {
                     PanCamera(Input.GetTouch(0).deltaPosition.x);
                 }
-                break;
-
-            case 2: // Zooming
-                Vector2[] newPositions = new Vector2[] { Input.GetTouch(0).position, Input.GetTouch(1).position };
-                if (!wasZoomingLastFrame)
-                {
-                    lastZoomPositions = newPositions;
-                    wasZoomingLastFrame = true;
-                }
-                else
-                {
-                    // Zoom based on the distance between the new positions compared to the 
-                    // distance between the previous positions.
-                   // float newDistance = Vector2.Distance(newPositions[0], newPositions[1]);
-                   // float oldDistance = Vector2.Distance(lastZoomPositions[0], lastZoomPositions[1]);
-                   // float offset = newDistance - oldDistance;
-
-                    ZoomCamera(5f, ZoomSpeedTouch);
-
-                    lastZoomPositions = newPositions;
-                }
-                break;
-
-            default:
-                wasZoomingLastFrame = false;
                 break;
         }
     }
@@ -208,98 +182,22 @@ public class MobileCameraControlBackup : MonoBehaviour
         {
             PanCamera(Input.GetAxis("Mouse X"));
         }
-
-        // Check for scrolling to zoom the camera
-        float scroll = Input.GetAxis("Mouse ScrollWheel");
-
-        ZoomCamera(scroll, ZoomSpeedMouse);
     }
 
     void PanCamera(float newPanPosition)
     {
         // Determine how much to move the camera
-       // Vector3 offset = cam.ScreenToWorldPoint(lastPanPosition - newPanPosition);
-       if (CurrentPosition == 2)
+        // Vector3 offset = cam.ScreenToWorldPoint(lastPanPosition - newPanPosition);
+        if (CurrentPosition == 2)
         {
             Vector3 move = new Vector3(0, newPanPosition * PanSpeed, 0);
 
             transform.Rotate(move, Space.World);
         }
 
-       // lastPanPosition = newPanPosition;
-    }
-
-    private void zoomInorOut()
-    {
-        OldFOV = cam.fieldOfView;
-    }
-
-    void ZoomCamera(float offset, float speed)
-    {
-        zoomtrue = true;
-        offset = 5f; //trying shit
-        if (offset == 0)
-        {
-            return;
-        }
-
-        if (CurrentFOV >= 79.5f && CurrentPosition == 0)
-        {
-            zoomInorOut();
-
-            cam.fieldOfView = Mathf.Clamp(cam.fieldOfView - (offset * speed), ZoomBounds[0], ZoomBounds[1]);
-
-            if (cam.fieldOfView > OldFOV)
-            {
-                ZoomingIn = false;
-                ZoomingOut = true;
-            }
-            else
-            {
-                ZoomingIn = true;
-                ZoomingOut = false;
-            }
-
-        }
-
-        if (CurrentFOV <= 37f && CurrentPosition == 1)
-        {
-            zoomInorOut();
-
-            cam.fieldOfView = Mathf.Clamp(cam.fieldOfView - (offset * speed), ZoomBounds[0], ZoomBounds[1]);
-
-            if (cam.fieldOfView > OldFOV)
-            {
-                ZoomingIn = false;
-                ZoomingOut = true;
-            }
-            else
-            {
-                ZoomingIn = true;
-                ZoomingOut = false;
-            }
-   
-        }
-
-        if (CurrentFOV >= 79.5f && CurrentPosition == 2)
-        {
-
-            cam.fieldOfView = Mathf.Clamp(cam.fieldOfView - (offset * speed), ZoomBounds[0], ZoomBounds[1]);
-
-                ZoomingIn = false;
-                ZoomingOut = true;
-
-        }
-
-
-
 
     }
-
-
 }
-
-
 
 
 
