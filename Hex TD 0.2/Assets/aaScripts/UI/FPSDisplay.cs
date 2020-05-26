da008c9bf6ec34506c96e4ab63be3299ee3128ea
Erzihark <63552221+Erzihark@users.noreply.github.com>
2020-05-26 11:22:42 -0500

    Tuned fps display

--- Hex TD 0.2/Assets/aaScripts/UI/FPSDisplay.cs
+++ Hex TD 0.2/Assets/aaScripts/UI/FPSDisplay.cs
@@ -5,13 +5,19 @@ public class FPSDisplay : MonoBehaviour
 {
     public int avgFrameRate;
     public Text display_Text;
+    private int interval = 6;
 
     public void Update()
     {
         float current = 0;
         current = (int)(1f / Time.unscaledDeltaTime);
-        avgFrameRate = (int)current;
-        display_Text.text = avgFrameRate.ToString() + " FPS";
+
+        if (Time.frameCount % interval == 0)
+        {
+            avgFrameRate = (int)current;
+            display_Text.text = avgFrameRate.ToString() + " FPS";
+        }
+        
     }
 }
 
