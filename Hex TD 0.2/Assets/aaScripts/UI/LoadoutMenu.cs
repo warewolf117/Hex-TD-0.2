80ea5647e35ad43536b0d6342978e7ad86fc9f8f
Erzihark <63552221+Erzihark@users.noreply.github.com>
2020-06-09 15:22:24 -0500

    Loadout WIP

---
+++ Hex TD 0.2/Assets/aaScripts/UI/LoadoutMenu.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadoutMenu : MonoBehaviour
+{
+    public ScreenFader screenFader;
+
+    public Button[] turretSelectButtons;
+
+    public int turretQuantity = 3;
+    private int turretsSelected = 0;
+
+    public Text numberTurretsSelected;
+
+    public static LevelEndMenu levelProgress;
+
+    private void Start()
+    {
+        selectableTurrets();      
+    }
+
+    void selectableTurrets()
+    {
+        turretsSelected ++;
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void SelectLevel(string levelName)
+    {
+        screenFader.FadeTo(levelName);
+    }
+
+
+}
